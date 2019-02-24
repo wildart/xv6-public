a5671959eae52c6bb500760333e08043bd3d2041
wildart <wildart@gmail.com>
2019-02-24 17:34:56 -0500

    part 1.1

--- syscall.c
+++ syscall.c
@@ -172,6 +172,9 @@ syscall(void)
   num = curproc->tf->eax;
   if(num > 0 && num < NELEM(syscalls) && syscalls[num]) {
     curproc->tf->eax = syscalls[num]();
+#ifdef PRINT_SYSCALLS
+    cprintf("%s -> %d\n", syscallnames[num], curproc->tf->eax);
+#endif // PRINT_SYSCALLS
   } else {
     cprintf("%d %s: unknown sys call %d\n",
             curproc->pid, curproc->name, num);
