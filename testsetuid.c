8fa4d20860153e2875c5700804bcb7083a90d4e5
Art Wild <wildart@gmail.com>
2019-03-30 01:55:07 -0400

    changed to a test from lab 2 manual

--- testsetuid.c
+++ testsetuid.c
@@ -2,10 +2,26 @@
 #include "types.h"
 #include "user.h"
 
-int
-main(int argc, char *argv[])
+int main ( void )
 {
-  printf(1, "***** In %s: my uid is %d\n\n", argv[0], getuid());
-  exit();
+    uint uid, gid, ppid;
+    uid = getuid();
+    printf( 2 , " Current UID is: %d\n", uid );
+    printf( 2 , " Setting UID to 100\n" );
+    setuid (100);
+    uid = getuid ();
+    printf( 2 , " Current UID is: %d\n", uid );
+
+    gid = getgid() ;
+    printf( 2 , " Current GID is: %d\n", gid );
+    printf( 2 , " Setting GID to 100\n" );
+    setgid (100);
+    gid = getgid();
+    printf( 2 , " Current GID is: %d\n", gid );
+
+    ppid = getppid();
+    printf( 2 , "My parent process is: %d\n", ppid );
+    printf( 2 , "Done!\n" );
+    exit();
 }
 #endif
